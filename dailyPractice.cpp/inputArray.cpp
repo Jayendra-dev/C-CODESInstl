a5c80605ea18c272bd57ba2da657ba9684a1943f
Jayendra Kumar <jayendrakumar.mca@gmail.com>
2026-08-26 19:10:40 +0530

    Add files via upload

---
+++ dailyPractice.cpp/inputArray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"enter no.of array elements you want:";
+    cin >> n;
+
+    int arr[100];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
