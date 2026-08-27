59bcc8f580da884a6faa978d12befe37e066a374
Jayendra Kumar <jayendrakumar.mca@gmail.com>
2026-08-27 23:56:22 +0530

    Add files via upload

---
+++ dailyPractice.cpp/check_even_oddUsingfunction.cpp
@@ -0,0 +1,17 @@
+//check even or odd
+#include<iostream>
+using namespace std;
+int even_odd(int n){
+	return n;
+}
+int main(){
+	int n;
+	cout<<"enter number:";
+	cin>>n;
+	if(n%2==0){
+		cout<<"even ";
+	}
+	else{
+		cout<<"odd";
+	}
+}
