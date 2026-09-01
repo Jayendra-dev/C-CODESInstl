0f6b75652d5deef9dea8d4d714ab7d29cd77bd0c
Jayendra Kumar <jayendrakumar.mca@gmail.com>
2026-09-02 00:04:50 +0530

    Implement function to check even or odd
    Added a function to check if a number is even or odd.

---
+++ dailyPractice.cpp/functions.cpp/maximum.cpp
@@ -0,0 +1,16 @@
+//write a funtion to find the maximum of two numbers
+#include <iostream>
+using namespace std;
+
+void check(int n)
+{
+    if(n%2==0)
+        cout<<"Even";
+    else
+        cout<<"Odd";
+}
+
+int main()
+{
+    check(9);
+}
