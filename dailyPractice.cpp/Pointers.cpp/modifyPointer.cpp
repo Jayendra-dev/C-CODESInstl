86fa44e7d81919f2c58690d0e22fa2ab666d7c37
Jayendra Kumar <jayendrakumar.mca@gmail.com>
2026-09-19 14:36:50 +0530

    Add files via upload

---
+++ dailyPractice.cpp/Pointers.cpp/modifyPointer.cpp
@@ -0,0 +1,9 @@
+//Modify a variable to 100 through its pointer without directly writing x = 100.
+#include<iostream>
+using namespace std;
+int main(){
+    int*n=nullptr;
+    *p=100;
+    cout<<p;
+    return 0;
+}
