9d9eb705deff1b504aacdc0ee58082e28fe42d23
Jayendra Kumar <jayendrakumar.mca@gmail.com>
2026-09-20 06:33:52 +0530

    Add files via upload

---
+++ dailyPractice.cpp/Pointers.cpp/level2Q5.cpp
@@ -0,0 +1,11 @@
+//Move a pointer forward using p++ and print each element
+#include<iostream>
+using namespace std;
+int main(){
+    int n[10]={1,2,3,4,5,6};
+    int*p=&n[0];
+    while (i<6){
+        p++;
+        cout<<p;
+    }
+}
